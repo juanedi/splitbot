f9c289125c30019fd63eb56ce4ac7ba69f69a109
Juan Edi <juan@noredink.com>
2018-08-04 19:46:50 -0300

    Basic migration setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Data.ByteString.Char8 (pack)
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.Migration
+import System.Environment (getEnv)
+import System.Exit (exitSuccess)
+
+data Config =
+  Config
+    { dbUrl :: String
+    }
+
+data CreateCommand =
+  CreateCommand
+    { payer      :: String
+    , budy       :: String
+    , payerShare :: Int
+    , amount     :: Int
+    }
 
 main :: IO ()
-main = someFunc
+main = do
+  migrateDB
+
+migrateDB :: IO ()
+migrateDB = do
+  putStrLn "--- Migrating database"
+  url <- getEnv "DB_URL"
+  conn <- connectPostgreSQL (pack url)
+  withTransaction conn $
+    runMigrations True conn $
+      [ MigrationInitialization
+      , (MigrationDirectory "./db/migrate/")
+      ]
+  return ()
+
+createExpense :: CreateCommand -> IO ()
+createExpense command =
+  return ()
