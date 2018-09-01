b9b429a93cb34fcd3cf1b3c6caae3efdd88b718d
Juan Edi <juan@noredink.com>
2018-09-01 11:19:21 -0300

    fix db column types

--- db/migrate/20180804182843-create-expenses.sql
+++ db/migrate/20180804182843-create-expenses.sql
@@ -1,7 +1,7 @@
 CREATE TABLE expenses (
-  id                 SERIAL PRIMARY KEY,
-  amount             INTEGER NOT NULL,
-  payer_share        VARCHAR(100) NOT NULL,
-  payer              VARCHAR(100) NOT NULL,
+  id                SERIAL PRIMARY KEY,
+  amount            INTEGER NOT NULL,
+  payer_share       INTEGER NOT NULL,
+  payer             VARCHAR(100) NOT NULL,
   peer              VARCHAR(100) NOT NULL
 );
