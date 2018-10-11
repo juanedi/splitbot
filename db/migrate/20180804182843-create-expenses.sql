411b304515d8ce330aff09c39623b0839ca618df
Juan Edi <jedi11235@gmail.com>
2018-10-11 17:53:53 -0300

    :skull: 'db' directory

--- db/migrate/20180804182843-create-expenses.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE expenses (
-  id                SERIAL PRIMARY KEY,
-  amount            INTEGER NOT NULL,
-  payer_share       INTEGER NOT NULL,
-  payer             VARCHAR(100) NOT NULL,
-  peer              VARCHAR(100) NOT NULL
-);
