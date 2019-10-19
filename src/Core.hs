e06747963addd86d887870a42c9300f4e49ddf34
Juan Edi <juan@noredink.com>
2019-10-19 12:09:24 -0300

    Read messages from queue and log them

--- src/Core.hs
+++ src/Core.hs
@@ -1,4 +1,4 @@
-module Core (initialize, update, Model, Event(..)) where
+module Core (initialize, update, Model, Event(..), Effect(..)) where
 
 import           Conversation.Parameters.Split (Split)
 import qualified Conversation.Parameters.Split as Split
@@ -8,7 +8,8 @@ import           Settings (Settings)
 import qualified Splitwise
 import qualified Telegram
 import           Telegram.Api (ChatId)
-import           Telegram.Message
+import           Telegram.Message (Message)
+import qualified Telegram.Message as Message
 import qualified Telegram.Username
 import           Telegram.Username (Username)
 
@@ -34,10 +35,15 @@ data ConversationState
   | -- we are waiting for a reply from the user
     Active ChatId Conversation
 
-data Event
-  = MessageReceived Telegram.Message.Message
+data UserId
+  = UserA
+  | UserB
 
-type Effect = ()
+data Event
+  = MessageReceived Message
+
+data Effect =
+  LogError String
 
 initialize :: Settings -> Model
 initialize settings
@@ -72,4 +78,61 @@ initialize settings
 
 update :: Event -> Model -> (Model, [Effect])
 update event model = case event of
-  MessageReceived _msg -> (model, [])
+  MessageReceived msg -> updateFromMessage msg model
+
+updateFromMessage :: Message -> Model -> (Model, [Effect])
+updateFromMessage msg model =
+  let username = Message.username msg
+  in  case matchUserId model username of
+        Nothing ->
+          ( model
+          , [ LogError
+                $  "Ignoring message from unknown user: "
+                ++ (show username)
+            ]
+          )
+        Just userId ->
+          let identifiedUsers = case userId of
+                UserA -> (getUser UserA model, getUser UserB model)
+                UserB -> (getUser UserB model, getUser UserB model)
+              (updatedCurrentUser, updatedPeer, effects) =
+                answerMessage msg identifiedUsers
+          in  ( (updateUser userId updatedCurrentUser)
+              . (updateUser (otherUserId userId) updatedPeer)
+              $ model
+              , effects
+              )
+
+answerMessage :: Message -> (User, User) -> (User, User, [Effect])
+answerMessage msg (currentUser, peer) =
+  -- TODO!
+  ( currentUser
+  , peer
+  , [ LogError
+        (  "Received message: "
+        ++ (Message.text msg)
+        ++ " from "
+        ++ (show (telegramId currentUser))
+        )
+    ]
+  )
+
+matchUserId :: Model -> Username -> Maybe UserId
+matchUserId model username | (telegramId . userA) model == username = Just UserA
+                           | (telegramId . userB) model == username = Just UserB
+                           | otherwise                              = Nothing
+
+otherUserId :: UserId -> UserId
+otherUserId userId = case userId of
+  UserA -> UserB
+  UserB -> UserA
+
+getUser :: UserId -> Model -> User
+getUser userId = case userId of
+  UserA -> userA
+  UserB -> userB
+
+updateUser :: UserId -> User -> Model -> Model
+updateUser userId user model = case userId of
+  UserA -> model { userA = user }
+  UserB -> model { userB = user }
--- src/Runtime.hs
+++ src/Runtime.hs
@@ -12,6 +12,18 @@ start settings queue = loop queue (Core.initialize settings)
 loop :: Queue Message -> Core.Model -> IO ()
 loop queue model = do
   msg <- Queue.dequeue queue
-  let event                    = Core.MessageReceived msg
-      (updatedModel, _effects) = Core.update event model
+  let event                   = (Core.MessageReceived msg)
+      (updatedModel, effects) = Core.update event model
+  runEffects effects
   loop queue updatedModel
+
+runEffects :: [Core.Effect] -> IO ()
+runEffects effects = case effects of
+  [] -> return ()
+  first : rest ->
+    -- TODO: catch errors
+    (runEffect first) >> runEffects rest
+
+runEffect :: Core.Effect -> IO ()
+runEffect effect = case effect of
+  Core.LogError msg -> putStrLn msg
