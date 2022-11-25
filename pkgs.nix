b49542c20a73f283300ec3ce21486acd9720f176
Juan Edi <juan@noredink.com>
2022-11-25 18:05:16 -0300

    :art:

--- pkgs.nix
+++ pkgs.nix
@@ -1,11 +1,9 @@
 let
   lockFile = builtins.fromJSON (builtins.readFile ./devenv.lock);
   pkgsSpec = lockFile.nodes.nixpkgs-stable.locked;
-  pkgsRev = pkgsSpec.rev;
-  pkgsHash = pkgsSpec.narHash;
 in
 import
   (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${pkgsRev}.tar.gz";
-    sha256 = pkgsHash;
+    url = "https://github.com/NixOS/nixpkgs/archive/${pkgsSpec.rev}.tar.gz";
+    sha256 = pkgsSpec.narHash;
   })
